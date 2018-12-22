a5f6227952b59a68fbc83f06787085055db94112
Michael <michael.freaney324@gmail.com>
2018-12-21 23:43:46 -0600

    Update textbox template

--- src/co/teruh/bot/commands/CommandSans.java
+++ src/co/teruh/bot/commands/CommandSans.java
@@ -7,25 +7,28 @@ import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 
 import co.teruh.bot.utils.UTTextbox;
+import net.dv8tion.jda.core.entities.Message;
 
 public class CommandSans extends Command {
 
 	private File image;
-	
+
 	public CommandSans() {
 		this.name = "sans";
 	}
-	
+
 	@Override
 	protected void execute(CommandEvent event) {
-		String text = event.getMessage().toString();
-		
+		Message eventMsg = event.getMessage();
+		String eventText = eventMsg.getContentDisplay();
+		String text = eventText.substring(eventText.indexOf("t.sans") + "t.sans".length());
+
 		try {
 			new UTTextbox(578, 152, text);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		image = new File("./res/textbox.png");
 		event.reply(image, "textbox.png");
 	}
