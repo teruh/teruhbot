8c08b0b53c61f7d36cb97a62eea7cdb5f7b1d50a
teruh <zachclarkbyte@gmail.com>
2018-12-23 18:00:10 -0500

    Add chief command

--- src/java/co/teruh/bot/commands/CommandChief.java
+++ src/java/co/teruh/bot/commands/CommandChief.java
@@ -8,16 +8,21 @@ import com.jagrosh.jdautilities.command.CommandEvent;
 public class CommandChief extends Command {
 	
 	public CommandChief() {
-		this.name = "is this it";
+		this.name = "it";
 	}
 
 	@Override
 	protected void execute(CommandEvent event) {
+		if (determine() == 1) {
+			event.reply("Chief says: this is it.");
+		} else {
+			event.reply("Chief says: this is not it.");
+		}
 	}
 	
 	private int determine() {
 		Random random = new Random();
-		int num = random.nextInt(1);
+		int num = random.nextInt(2);
 		return num;
 	}
 }
